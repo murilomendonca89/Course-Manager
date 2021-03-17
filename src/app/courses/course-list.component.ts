4386e22df4f7725d5879dacd2065dda21f7273a9
murilomendonca89 <murilomendonca@live.com>
2021-03-16 21:53:15 -0300

    Criação de modulos, componentes e atributos

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core'
+import { Course } from './course'
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = []
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'May, 2, 2021'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
+                price: 45.99,
+                code: 'LKL-194',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'March, 29, 2021'
+            }
+        ]
+    }
+
+}
